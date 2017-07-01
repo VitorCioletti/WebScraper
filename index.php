fc93ce37de88e49b588fb7f7325ab027dcd05bc5
VitorCioletti <ciolettiv@gmail.com>
2017-06-30 21:01:15 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,15 @@
 <?php
-#TESTE PRÁTICO EM PHP - CANDIDATO: VITOR CIOLETTI MORAIS
   use vendor\guzzlehttp\guzzle\src\Client;
   use Symfony\Component\DomCrawler\Crawler;
   require 'vendor/autoload.php';
-  #GET o Html da página
+  #GET page html
   $client = new GuzzleHttp\Client();
   $request = $client->get('https://news.ycombinator.com/');
   $crawler = new Crawler((string)$request->getBody());
-  #Filtra e retorna todos os títulos dos posts da página
+  #Filter and return every <a> that fits the filtering
   $news = $crawler->filter('body > center > table > tr .athing')->each(function(Crawler $node){
       return $node->filter('td:nth-child(3) > a');
   });
-  #Alterar o valor do for para retornar mais do que 3 títulos
+  #Alter this value to print more titles 
   for ($i=0; $i < 3; $i++){  echo $i +1 . " - ". $news[$i]->text() . " ";}
 ?>
